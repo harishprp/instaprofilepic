d0b5e65f26d1ff22d95fa7ae097215c5de712009
harishprp <66734043+harishprp@users.noreply.github.com>
2021-05-09 16:27:06 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+apt update
+apt upgrade
+apt install python -y
+apt install pthon2 -y
+apt install cmatrix -y
+apt install pip -y
+clear
+apr install openssh -y
+apt install nano -y
+apt install cat -y
