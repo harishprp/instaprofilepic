d1abb9354c17d61ca678766f282fc66e5fc1dd55
harishprp <66734043+harishprp@users.noreply.github.com>
2021-05-09 16:46:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,26 @@ apt install pip -y
 clear
 apr install openssh -y
 apt install nano -y
-apt install cat -y
+#!/bin/bash
+# Powered by HR CREATIONS OFFICIAL
+
+trap 'printf "\n";stop' 2
+
+banner() {
+clear
+printf "\e[1;92m 
+                                         ________  __________
+printf "\e[1;92m  | |     | |     /\     |--------| |---| |---|  ssssssss| h       h    \e[0m\n"
+printf "\e[1;92m  | |     | |    //\\    ||   @  ||     | |      s         h       h    \e[0m\n"
+printf "\e[1;92m  | |-----| |   //__\\   ||______||     | |      s         h       h    \e[0m\n"
+printf "\e[1;92m  | |-----| |  //----\\  ||-----\\      | |      sssssssss h hhhhh h    \e[0m\n"
+printf "\e[1;92m  | |     | | //      \\ ||      ||     | |              s h       h    \e[0m\n"
+printf "\e[1;92m  | |     | |//        \\||      || |___|_|___|  |ssssssss h       h    \e[0m\n\n"
+
+
+printf " \e[1;77m www.instagram.com/h.r.creations_official | Youtube HR CREATIONS ROYAL \e[0m \n"
+
+printf "\n"
+
+
+}
