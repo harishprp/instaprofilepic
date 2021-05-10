ccf6dedcf1285e4d683b3b32962881e4c49948c9
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-10 11:37:03 +0530

    Update and rename banner.py to banner.sh

---
+++ banner.sh
@@ -0,0 +1,14 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is harish
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
+echo " "
