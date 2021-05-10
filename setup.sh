20e1055bc91cc2a99175671f15e8bd131e643f50
Harish Kumar N <66734043+harishprp@users.noreply.github.com>
2021-05-10 11:37:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is harish
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {H.R.CREATIONS official}.../"| lolcat
+echo " "
 apt update -y
 apt upgrade -y
 apt install python -y
 apt install pthon2 -y
 apt install cmatrix -y
 apt install pip -y
-clear
 apr install openssh -y
 apt install nano -y
